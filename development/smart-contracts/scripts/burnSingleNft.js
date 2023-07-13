30ed4e1dd66b6bd87a96cc37f25511c8c02822a3
marc0olo <walz.marcoo@gmail.com>
2023-07-14 01:31:36 +0200

    add script to burn nft

---
+++ development/smart-contracts/scripts/burnSingleNft.js
@@ -0,0 +1,53 @@
+require('dotenv').config({ path: '.env.local' });
+const { AeSdk, CompilerHttp, MemoryAccount, Node } = require('@aeternity/aepp-sdk');
+
+const shutdown = (varName) => {
+    console.error(`Missing ENV variable: ${varName}`);
+    process.exit(1);
+}
+
+if(!process.env.SECRET_KEY_ACCOUNT_1) {
+    shutdown('SECRET_KEY_ACCOUNT_1');
+}
+
+if(!process.env.CONTRACT_ID) {
+    shutdown('CONTRACT_ID');
+}
+
+if(!process.env.NFT_ID) {
+    shutdown('NFT_ID');
+}
+
+const compilerUrl = process.env.COMPILER_URL ? process.env.COMPILER_URL : 'http://localhost:3080';
+const contractId = process.env.CONTRACT_ID;
+
+const nftId = process.env.NFT_ID;
+
+// run 'generateBytecodeAndAci.js' first
+const aci = require('../generated_artifacts/aci.json');
+
+const AE_NETWORK = process.env.AE_NETWORK || 'TESTNET'
+const SETTINGS = {
+    TESTNET: {
+        nodeUrl: 'https://testnet.aeternity.io'
+      },
+    MAINNET: {
+        nodeUrl: 'https://mainnet.aeternity.io'
+    }
+}
+
+const main = async () => {
+    const node = new Node(SETTINGS[AE_NETWORK].nodeUrl);
+    const aeSdk = new AeSdk({
+        onCompiler: new CompilerHttp(compilerUrl),
+        nodes: [
+          { name: AE_NETWORK, instance: node },
+        ],
+        accounts: [new MemoryAccount(process.env.SECRET_KEY_ACCOUNT_1)],
+    });
+
+    const contract = await aeSdk.initializeContract({ aci, address: contractId });
+    await contract.burn(nftId);
+}
+
+main();
