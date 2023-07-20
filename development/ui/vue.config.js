14f5340cbe3ee6744445ab2ad6c8aec4464a7da8
marc0olo <walz.marcoo@gmail.com>
2023-07-20 10:26:28 +0200

    build with correct env variables

--- development/ui/vue.config.js
+++ development/ui/vue.config.js
@@ -1,19 +1,6 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/aepp-boilerplate-vue/'
-    : '/',
+  publicPath: '/',
   transpileDependencies: [
     '@aeternity'
   ],
-  // added file-loader
-  chainWebpack: config => {
-    // .aes loader
-    config.module
-      .rule('aes')
-      .test(/\.aes$/)
-      .use('file-loader')
-      .loader('file-loader')
-      .end()
-  }
-
 }
