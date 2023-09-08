8c1501ca84f28ed15f7dd238d40cb5ae9db1b72a
marc0olo <walz.marcoo@gmail.com>
2023-09-08 15:39:59 +0200

    Sophia compiler v7.4.0, introduce alternative compilation via CLI

--- development/smart-contracts/scripts/generateBytecodeAndAci-ceres.js
+++ development/smart-contracts/scripts/generateBytecodeAndAci-ceres.js
@@ -1,19 +1,40 @@
 require('dotenv').config({ path: '.env.local' });
 const fs = require('fs');
+const execSync = require('child_process').execSync;
 const { AeSdk, CompilerHttp } = require('@aeternity/aepp-sdk');
 const { utils } = require('@aeternity/aeproject');
 
-const CONTRACT = '../contracts/AENSWrappingCeres.aes';
-const sourceCode = utils.getContractContent(CONTRACT);
-const fileSystem = utils.getFilesystem(CONTRACT);
-
-const compilerUrl = process.env.COMPILER_CERES_URL ? process.env.COMPILER_CERES_URL : 'http://localhost:3081';
+const compiler = process.env.COMPILER;
 
 const main = async () => {
+    switch(compiler) {
+        case 'HTTP':
+            await runCompilerHttp();
+            break;
+        case 'CLI':
+        default:
+            await runCompilerCli();
+            break;
+    }
+}
+
+const runCompilerCli = async () => {
+    const output = execSync('./../aesophia_cli/v7.4.0 ../contracts/AENSWrappingCeres.aes -i ../contracts -i ../contracts/interfaces -o ../generated_artifacts/bytecode-ceres', { encoding: 'utf-8' });
+    console.log('Output was:\n', output);
+    // TODO aci
+}
+
+const runCompilerHttp = async () => {
+    const CONTRACT = '../contracts/AENSWrappingCeres.aes';
+    const sourceCode = utils.getContractContent(CONTRACT);
+    const fileSystem = utils.getFilesystem(CONTRACT);
+
+    const compilerUrl = process.env.COMPILER_CERES_URL ? process.env.COMPILER_CERES_URL : 'http://localhost:3081';
     const aeSdk = new AeSdk({
         onCompiler: new CompilerHttp(compilerUrl),
     });
     const contract = await aeSdk.initializeContract({ sourceCode, fileSystem });
+    console.log(contract);
     fs.writeFile('../generated_artifacts/aci-ceres.json', JSON.stringify(contract._aci), (err) => {
         if (err) throw err;
     });
--- development/smart-contracts/scripts/generateBytecodeAndAci-iris.js
+++ development/smart-contracts/scripts/generateBytecodeAndAci-iris.js
@@ -1,15 +1,34 @@
 require('dotenv').config({ path: '.env.local' });
 const fs = require('fs');
+const execSync = require('child_process').execSync;
 const { AeSdk, CompilerHttp } = require('@aeternity/aepp-sdk');
 const { utils } = require('@aeternity/aeproject');
 
-const CONTRACT = '../contracts/AENSWrapping.aes';
-const sourceCode = utils.getContractContent(CONTRACT);
-const fileSystem = utils.getFilesystem(CONTRACT);
-
-const compilerUrl = process.env.COMPILER_URL ? process.env.COMPILER_URL : 'http://localhost:3080';
+const compiler = process.env.COMPILER;
 
 const main = async () => {
+    switch(compiler) {
+        case 'HTTP':
+            await runCompilerHttp();
+            break;
+        case 'CLI':
+        default:
+            await runCompilerCli();
+            break;
+    }
+}
+
+const runCompilerCli = async () => {
+    execSync('./../aesophia_cli/v7.4.0 ../contracts/AENSWrapping.aes -i ../contracts -i ../contracts/interfaces -o ../generated_artifacts/bytecode-iris', { encoding: 'utf-8' });
+    execSync('./../aesophia_cli/v7.4.0 --create_json_aci ../contracts/AENSWrapping.aes -i ../contracts -i ../contracts/interfaces -o ../generated_artifacts/aci-iris.json', { encoding: 'utf-8' });
+}
+
+const runCompilerHttp = async () => {
+    const CONTRACT = '../contracts/AENSWrapping.aes';
+    const sourceCode = utils.getContractContent(CONTRACT);
+    const fileSystem = utils.getFilesystem(CONTRACT);
+
+    const compilerUrl = process.env.COMPILER_URL ? process.env.COMPILER_URL : 'http://localhost:3080';
     const aeSdk = new AeSdk({
         onCompiler: new CompilerHttp(compilerUrl),
     });
