b75f3cab916ca7dd730b3c9b1330b4c76b14c402
marc0olo <walz.marcoo@gmail.com>
2023-09-12 16:24:17 +0200

    test to skip failing gh-pages preview build on vercel

--- development/ui/vercel-build-command.sh
+++ development/ui/vercel-build-command.sh
@@ -3,7 +3,10 @@
 if [[ $VERCEL_GIT_COMMIT_REF == "master"  ]] ; then 
   echo "Production build (Mainnet)"
   yarn build:mainnet
-else # should be develop branch
+elif [[ $VERCEL_GIT_COMMIT_REF == "develop"  ]] ; then 
   echo "Preview Build (Testnet)"
   yarn build:testnet
+else
+  echo "Skipping Build"
+  # do nothing
 fi
