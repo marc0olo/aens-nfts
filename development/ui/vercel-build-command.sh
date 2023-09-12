dbdde7f788ff6cda78d78acfe47574c598df2fac
marc0olo <walz.marcoo@gmail.com>
2023-09-12 16:18:35 +0200

    master branch instead of main branch ...

--- development/ui/vercel-build-command.sh
+++ development/ui/vercel-build-command.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [[ $VERCEL_GIT_COMMIT_REF == "main"  ]] ; then 
+if [[ $VERCEL_GIT_COMMIT_REF == "master"  ]] ; then 
   echo "Production build (Mainnet)"
   yarn build:mainnet
 else # should be develop branch
